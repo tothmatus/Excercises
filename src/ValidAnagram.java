68834da78e54bcf514f0f35a357e5bca25737e36
MatusToth <toth.matus@outlook.com>
2023-03-09 19:40:52 +0100

    Anagram

---
+++ src/ValidAnagram.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import main.StopWatch;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        List<String[]> testCase = List.of(
+            new String[] { "anagram", "nagaram" }, 
+            new String[] { "anagrame", "nagarame" }, 
+            new String[] { "anagrame", "nagarami" }, 
+            new String[] { "anagram", "nagaramn" }
+        );
+
+        testCase.forEach(t -> {
+            boolean result;
+            
+            StopWatch.start();
+            result = isAnagram(t[0], t[1]);
+            StopWatch.finishReport();
+            System.out.println("isAnagram: " + result);
+    
+            StopWatch.start();
+            result = isAnagramAlternative(t[0], t[1]);
+            StopWatch.finishReport();
+            System.out.println("AnagramAlternative: " + result);
+        });
+
+    }
+
+    private static boolean isAnagram(String s, String t) {
+        if(s.length() == t.length()){
+            char[] sArray = s.toCharArray();
+            char[] tArray = t.toCharArray();
+            Arrays.sort(sArray);
+            Arrays.sort(tArray);
+            return Arrays.equals(sArray, tArray);
+        }
+        return false;
+    }
+
+    private static boolean isAnagramAlternative(String s, String t) {
+        if(s.length() == t.length()){
+            int[] frequency = new int[26];
+            for(int i = 0; i < s.length(); i++){
+                int letterS = s.charAt(i) - 'a';
+                int letterT = t.charAt(i) - 'a';
+                frequency[letterS]++;
+                frequency[letterT]--;
+            }
+            return Arrays.stream(frequency).sum() == 0;
+        }
+        return false;
+    }
+}
--- src/main/StopWatch.java
+++ src/main/StopWatch.java
@@ -2,11 +2,17 @@ package main;
 
 public class StopWatch {
     private static long startTime;
+    private static long endTime;
     public StopWatch() {}
     public static void start(){
         startTime = System.nanoTime();
     }    
     public static long finish(){
         return System.nanoTime() - startTime;
+    }
+    public static void finishReport(){
+        endTime = System.nanoTime() - startTime;
+        System.out.println("\nMethod finished in: " + endTime +" ns");
+
     }
 }
